92f6d1982a2974f2f430d3325e8ecde78a2ce5dd
lizzieinvancouver <lizzie@biodiversity.ubc.ca>
2022-09-21 10:36:58 +0200

    Adding some code that may be useful to others

---
+++ helpfulcode/doysouthernhemisphere.R
@@ -0,0 +1,18 @@
+## Started on my brother's birthday, 2022 ##
+## Code to translate doy in northern and southern hemispheres ##
+
+# BTW, how get DOY for nothern hemisphere
+# df$doy <- as.numeric(format(df$date , "%j"))
+
+# Here we'll use DOY to get a sequence of dates
+nhemi <- c(1:365)
+nhemidate <- as.Date(paste(nhemi, 2002, sep="-"), format="%j-%Y") # I picked a random non-leap year
+
+# Now, we start counting on 1 July for southern hemisphere
+shemi <- c(185:365, 1:184)
+
+# Make a look up table
+northsouth <- data.frame(caldate=nhemidate, northdoy=nhemi, southdoy=shemi)
+
+# Write it out to your location
+write.csv(northsouth, "/Users/lizzie/Documents/git/projects/vin/adelaideclimate/analyses/output/northsouthlookup.csv", row.names=FALSE)
---
+++ helpfulcode/install_allpackages.R
@@ -0,0 +1,15 @@
+## Save and re-install your packages when updating R ##
+
+# 1. From the old R:
+tmp <- installed.packages()
+installedpkgs <- as.vector(tmp[is.na(tmp[,"Priority"]), 1])
+save(installedpkgs, file="installed_old.rda")
+
+# 2. Install new R and run:
+
+load("installed_old.rda")
+tmp <- installed.packages()
+installedpkgs.new <- as.vector(tmp[is.na(tmp[,"Priority"]), 1])
+missing <- setdiff(installedpkgs, installedpkgs.new)
+install.packages(missing)
+update.packages()
